ef180c0da5d5f64677950384ea048a01d9c540ca
Luca Hennet <luca.hennet@students.unibe.ch>
2025-11-24 17:42:03 +0100

    Rename script and add comments

--- scripts/01.1-run_TE_annotation.sh
+++ scripts/01.1-run_TE_annotation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 #SBATCH --job-name=EDTA_annotation
 #SBATCH --partition=pibu_el8
@@ -16,12 +16,13 @@ OUTDIR=$WORKDIR/results/EDTA_annotation
 mkdir -p $OUTDIR
 cd "$OUTDIR"
 
+# Processing of the Hifiasm-assembled genome FASTA file and use a set of known CDS (Coding Sequence) data from Arabidopsis thaliana (TAIR10) as a reference for distinguishing TEs from protein-coding genes.
 apptainer run --bind /data $CONTAINER \
     EDTA.pl \
     --genome $INPUT_FASTA \
-    --species others \
-    --step all \
-    --sensitive 1 \
-    --cds "/data/courses/assembly-annotation-course/CDS_annotation/data/TAIR10_cds_20110103_representative_gene_model_updated" \
-    --anno 1 \
+    --species others \ # general mode
+    --step all \ # run all steps of the EDTA pipeline
+    --sensitive 1 \ # enable sensitive mode for better detection
+    --cds "/data/courses/assembly-annotation-course/CDS_annotation/data/TAIR10_cds_20110103_representative_gene_model_updated" \ # reference coding Sequence to filter out protein-coding genes.
+    --anno 1 \ # final annotation step to generate the non-redundant TE library and annotation file
     --threads $SLURM_CPUS_PER_TASK
